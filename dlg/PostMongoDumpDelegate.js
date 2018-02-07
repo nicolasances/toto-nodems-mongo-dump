c4930dc07f5180c548f3b6a8334827c8f4e17263
nicolas <nicolas.matteazzi@gmail.com>
2018-02-07 22:27:37 +0100

    Correct usage of env var

--- dlg/PostMongoDumpDelegate.js
+++ dlg/PostMongoDumpDelegate.js
@@ -15,6 +15,8 @@ exports.postDump = function(postDumpRequest) {
 
     var dumpTS = moment().tz('Europe/Rome').format('YYYYMMDDHHmmss');
     var dumpname = 'totodump-' + dumpTS + '.tgz';
+    var env = postDumpRequest.env;
+    var gitFolder = '/' + env + '-mongo-dump';
 
     exec('mongodump --host mongo:27017 -o /mongo-dump', function(err, stdout, stderr) {
 
@@ -26,12 +28,12 @@ exports.postDump = function(postDumpRequest) {
         console.log(stdout);
         console.log(stderr);
 
-        exec('rm -r /' + env + '-mongo-dump; git clone https://' + a + ':' + b + '@gitlab.com/totoances/' + postDumpRequest.env + '-mongo-dump.git; mv ' + dumpname + ' /' + env + '-mongo-dump', function(err, stdout, stderr) {
+        exec('rm -r ' + gitFolder + '; git clone https://' + a + ':' + b + '@gitlab.com/totoances/' + postDumpRequest.env + '-mongo-dump.git; mv ' + dumpname + ' ' + gitFolder, function(err, stdout, stderr) {
 
           console.log(stdout);
           console.log(stderr);
 
-          exec('cd /' + env + '-mongo-dump; git init; git config --global user.email "toto.matteazzi@gmail.com"; git config --global user.name "totoances"; git add ' + dumpname + '; git commit -m \'Backup\'; git push --set-upstream https://' + a + ':' + b + '@gitlab.com/totoances/' + postDumpRequest.env + '-mongo-dump.git master;', function(err, stdout, stderr) {
+          exec('cd ' + gitFolder + '; git init; git config --global user.email "toto.matteazzi@gmail.com"; git config --global user.name "totoances"; git add ' + dumpname + '; git commit -m \'Backup\'; git push --set-upstream https://' + a + ':' + b + '@gitlab.com/totoances/' + postDumpRequest.env + '-mongo-dump.git master;', function(err, stdout, stderr) {
 
             console.log(stdout);
             console.log(stderr);
