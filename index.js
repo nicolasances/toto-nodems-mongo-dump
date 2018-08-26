e7f0374cc35a964507a26227f61e16fa61dfd50f
nicolas <nicolas.matteazzi@gmail.com>
2018-08-26 11:04:17 +0200

    Removed failed dependencies

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ var bodyParser = require("body-parser");
 var logger = require('toto-apimon-events')
 
 var postMongoDumpDlg = require('./dlg/PostMongoDumpDelegate');
-var getLastDumpDlg = require('./dlg/GetLastDumpDelegate');
 
 var apiName = 'mongo-dump';
 
@@ -21,7 +20,6 @@ app.use(express.static('/app'));
 
 app.get('/', function(req, res) {res.send({api: apiName, status: 'running'});});
 app.post('/dumps', function(req, res) {logger.apiCalled(apiName, '/dumps', 'POST', req.query, req.params, req.body); postMongoDumpDlg.postDump(req.body).then(function(result) {res.send(result);});});
-app.get('/dumps/last', function(req, res) {logger.apiCalled(apiName, '/dumps/last', 'GET', req.query, req.params, req.body); getLastDumpDlg.getLastDump().then(function(result) {res.send(result);});});
 
 app.listen(8080, function() {
   console.log('Mongo Dump Microservice up and running');
