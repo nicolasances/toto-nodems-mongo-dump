4976f606a36a7f7e9b6290fdc6733843b2be5a1d
nicolas <nicolas.matteazzi@gmail.com>
2018-08-26 10:52:34 +0200

    Authorization header accepted

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var bodyParser = require("body-parser");
 var logger = require('toto-apimon-events')
 
 var postMongoDumpDlg = require('./dlg/PostMongoDumpDelegate');
+var getLastDumpDlg = require('./dlg/GetLastDumpDelegate');
 
 var apiName = 'mongo-dump';
 
@@ -11,7 +12,7 @@ var app = express();
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, GoogleIdToken");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   res.header("Access-Control-Allow-Methods", "OPTIONS, GET, PUT, POST, DELETE");
   next();
 });
@@ -20,6 +21,7 @@ app.use(express.static('/app'));
 
 app.get('/', function(req, res) {res.send({api: apiName, status: 'running'});});
 app.post('/dumps', function(req, res) {logger.apiCalled(apiName, '/dumps', 'POST', req.query, req.params, req.body); postMongoDumpDlg.postDump(req.body).then(function(result) {res.send(result);});});
+app.get('/dumps/last', function(req, res) {logger.apiCalled(apiName, '/dumps/last', 'GET', req.query, req.params, req.body); getLastDumpDlg.getLastDump().then(function(result) {res.send(result);});});
 
 app.listen(8080, function() {
   console.log('Mongo Dump Microservice up and running');
